ff431359fec899a39a7f6d46b93177024d1396b0
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2019-08-31 22:30:29 -0400

    v.02, mobile layout

---
+++ index.js
@@ -0,0 +1,40 @@
+// Joseph P. Pasaoa
+//
+
+const debug = (logThis) => {
+  console.log(logThis);
+}
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const nameEntry = (userInput) => {
+
+  }
+
+  let user-one
+
+
+} );
+
+/* PLANNING
+
+process:
+Enter Player 1 Name
+Enter Player 2 Name
+Random choose who goes first
+Game Engine
+Save Records
+Loser Starts Next Round?
+Restart Round Mech
+New Match Mech
+
+secondary add-ons:
+Visual Editing
+Sound Editing
+
+future its: vs Computer/AI, rotated keyboard for player 2 name entry
+
+
+
+*/
